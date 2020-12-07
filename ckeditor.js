c72f6c8d92653c39417a7387b6637dc07b5ac973
Norman Privaldos, Jr <normanprivaldosjr@gmail.com>
2020-12-07 16:44:44 +0800

    🐛  Test add small format

--- ckeditor.js
+++ ckeditor.js
@@ -68,11 +68,13 @@ function initializeCKEditor(element, basePath = '/kontent-custom-element-ckedito
 
   config.removeButtons = 'Source,Save,NewPage,ExportPdf,Preview,Print,Templates,Cut,Copy,Paste,PasteText,PasteFromWord,Undo,Redo,Find,Replace,SelectAll,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Strike,Subscript,CopyFormatting,RemoveFormat,Outdent,Indent,CreateDiv,JustifyLeft,JustifyCenter,JustifyRight,JustifyBlock,BidiLtr,BidiRtl,Language,Anchor,Flash,HorizontalRule,Smiley,SpecialChar,PageBreak,Iframe,Font,FontSize,TextColor,BGColor,Maximize,ShowBlocks,About,Styles';
 
-  config.format_tags = 'p;h2';
+  config.format_tags = 'p;h2;small';
+  CKEDITOR.config.format_small = {element: 'small'};
 
   CKEDITOR.lang.load('en', 'en', function() {
     CKEDITOR.lang.en.format.tag_h2 = 'Heading';
     CKEDITOR.lang.en.format.tag_p = 'Paragraph';
+    CKEDITOR.lang.en.format.tag_small = 'Small';
   });
 
   /* load toolbar config from element settings if present */

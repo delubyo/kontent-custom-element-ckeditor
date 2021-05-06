c47fe186f177e760ed0b43b4cb003266afa002a2
Norman Privaldos, Jr <normanprivaldosjr@gmail.com>
2021-05-06 16:38:27 +0800

    🐛  Reenable outdent

--- ckeditor.js
+++ ckeditor.js
@@ -81,7 +81,7 @@ function initializeCKEditor(element, basePath = '/kontent-custom-element-ckedito
 		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
 		{ name: 'forms', groups: [ 'forms' ] },
 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'outdent', 'blocks', 'align', 'bidi', 'paragraph' ] },
 		{ name: 'links', groups: [ 'links' ] },
 		{ name: 'insert', groups: [ 'insert' ] },
 		{ name: 'styles', groups: [ 'styles' ] },
